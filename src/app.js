b00e2e5ba7fc61cf7671fd2229c7c2ad6c4edd10
ksushapopova <ks.popova.2005@mail.ru>
2026-09-17 14:40:57 +0300

    feat(app): wire requestId, notFound and errorHandler in order

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import { router } from './routes/index.js';
+import { requestId } from './middlewares/requestId.js';
+import { notFound } from './middlewares/notFound.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export function createApp() {
   const app = express();
 
+  app.use(requestId);
+  
   app.use(express.json({ limit: '100kb' }));
 
   app.use('/api', router);
+
+  app.use(notFound);
+
+  app.use(errorHandler);
 
   return app;
 }
